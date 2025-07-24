da7383ffc952d54618003d633d997734ec7e3912
singh7970 <priy2032004anshu.com>
2025-07-24 15:22:19 +0530

    add requirnment fileisdsssss r

--- mail_automation/settings.py
+++ mail_automation/settings.py
@@ -76,15 +76,15 @@ WSGI_APPLICATION = 'mail_automation.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': os.getenv("NAME"),
+    #     'USER': os.getenv("USER"),
+    #     'PASSWORD': os.getenv("PASSWORD"),
+    #     'HOST': os.getenv("HOST") or 'localhost',
+    #     'PORT': os.getenv("PORT") or '3306',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("NAME"),
-        'USER': os.getenv("USER"),
-        'PASSWORD': os.getenv("PASSWORD"),
-        'HOST': os.getenv("HOST") or 'localhost',
-        'PORT': os.getenv("PORT") or '3306',
-    },
-    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
