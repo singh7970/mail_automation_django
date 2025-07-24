23b5b522c23da86ca0b47b751ab28826d2730a12
singh7970 <priy2032004anshu.com>
2025-07-24 15:03:06 +0530

    add requirnment fileisdsss

--- mail_automation/settings.py
+++ mail_automation/settings.py
@@ -88,14 +88,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-    'postgres': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'your_postgres_db',
-        'USER': 'your_postgres_user',
-        'PASSWORD': 'your_postgres_password',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    # 'postgres': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'your_postgres_db',
+    #     'USER': 'your_postgres_user',
+    #     'PASSWORD': 'your_postgres_password',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
 }
 
 AUTH_USER_MODEL = 'app.Register'
